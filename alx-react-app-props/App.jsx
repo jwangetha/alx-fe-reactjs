f9bb84ca4ad0dbd5d324f90812dfef2a56142170
jwangetha <jwangetha@gmail.com>
2025-11-08 17:39:53 +0300

    fix: use Context API in UserProfile and remove props

--- alx-react-app-props/App.jsx
+++ alx-react-app-props/App.jsx
@@ -1,5 +1,5 @@
-import UserProfile from './components/UserProfile';
 import UserContext from './context/UserContext';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
