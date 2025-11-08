8d5917f0e60925ccd0c83c84751617e439d816d0
jwangetha <jwangetha@gmail.com>
2025-11-08 12:03:34 +0300

    Alterations

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,7 @@
-import React from 'react';
+// src/UserContext.js
+import React from "react";
 
+// Create a context for user data
 const UserContext = React.createContext();
 
 export default UserContext;
